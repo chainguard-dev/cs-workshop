b62cd04dd24c8debc8cfe22e4d2f537d47aeca80
Eric Smalling <eric.smalling@chainguard.dev>
2024-12-04 23:20:58 -0600

    add chainctl image list to dtDemo

--- train-the-trainer/developer-track/dtDemo.sh
+++ train-the-trainer/developer-track/dtDemo.sh
@@ -2,7 +2,7 @@
 
 PS3="Select item to run: "
 
-items=("Tag History API" "chainctl images diff" "crane - list image tags" "grype - scan image" "sfyt - create SBOM for image" "cosign - download SBOM")
+items=("Tag History API" "chainctl images diff" "chainctl images list" "crane - list image tags" "grype - scan image" "sfyt - create SBOM for image" "cosign - download SBOM")
 
 while true; do
     select item in "${items[@]}" Quit
@@ -26,25 +26,34 @@ while true; do
                echo "Running chainctl images diff cgr.dev/chainguard/$image1 cgr.dev/chainguard/$image2"
                (chainctl images diff cgr.dev/chainguard/$image1 cgr.dev/chainguard/$image2 | jq)
                break;;
+            #Chainguard - chainctl images list
             3) echo "Run $item";
+               echo "Enter org name (i.e. acme.com)"
+               read -r orgName
+               echo "Enter image repo name (i.e. python)"
+               read -r imageName
+               echo "Running chainctl images list --repo $imageName --parent $orgName --show-epochs"
+               (chainctl images list --repo $imageName --parent $orgName --show-epochs)
+               break;;
+            4) echo "Run $item";
                echo "Enter image name (i.e. jdk)"
                read -r imageName 
                echo "Running crane ls -O cgr.dev/chainguard-private/$imageName"
                (crane ls -O cgr.dev/chainguard-private/$imageName)
                break;;
-            4) echo "Selected item #$REPLY which means $item";
+            5) echo "Selected item #$REPLY which means $item";
                echo "Enter image name and tag (i.e. jdk:latest)"
                read -r imageTag 
                echo "grype cgr.dev/chainguard/$imageTag"
                (grype cgr.dev/chainguard/$imageTag)
                break;;
-            5) echo "Selected item #$REPLY which means $item";
+            6) echo "Selected item #$REPLY which means $item";
                echo "Enter image name and tag (i.e. jdk:latest)"
                read -r imageName
                echo "Running syft cgr.dev/chainguard/$imageName -o syft-text "
                (syft cgr.dev/chainguard/$imageName -o syft-text)
                break;;
-            6) echo "Selected item #$REPLY which means $item";
+            7) echo "Selected item #$REPLY which means $item";
                echo "Enter image name and tag (i.e. jdk:latest)"
                read -r imageName
                echo "Running cosign download attestation cgr.dev/chainguard/$imageName | jq -r .payload | base64 -d | jq .predicate"

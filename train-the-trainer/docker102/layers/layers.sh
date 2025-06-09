ff4508d18e1779ecdba8fb70af00dcf328f00b64
Rob Best <rob.best@chainguard.dev>
2025-06-09 17:38:06 +0100

    docker102: fix multi stage demo and add base-chroot (#25)
    The example was throwing errors because we weren't copying all the
    required libraries for `jq`.
    
    I was also finding it hard to remember all the steps to check the
    dependencies of `jq` with `apk info -L` and `ldd`, so I've explicitly
    added them to the demo.
    
    Also tweak the layer demo slightly to show diffs with `git`.

--- train-the-trainer/docker102/layers/layers.sh
+++ train-the-trainer/docker102/layers/layers.sh
@@ -6,17 +6,16 @@ for tag in $TAGS; do
 done
 clear
 banner "Step 1: Get a big file, untar it, and remove the tarball"
-$BATCAT Dockerfile.1
+pe "$BATCAT Dockerfile.1"
 pe "docker build . -t layers:1 -f Dockerfile.1"
 pe "docker images layers"
 pe "docker image history layers:1"
 wait
 
 banner "Step 2: Concatenate operations into a single RUN line"
-$BATCAT Dockerfile.2
+pe "git diff --no-index -U1000 Dockerfile.1 Dockerfile.2"
 pe "docker build . -t layers:2 -f Dockerfile.2"
 pe "docker images layers"
-pe "diff Dockerfile.1 Dockerfile.2"
 
 pe "docker image history layers:1"
 pe "docker image history layers:2"
--- train-the-trainer/docker102/multistage/multistage.sh
+++ train-the-trainer/docker102/multistage/multistage.sh
@@ -14,7 +14,7 @@ done
 
 clear
 banner "Step 1: Using multistage to eliminate unwanted layers"
-$BATCAT Dockerfile.1
+pe "git diff --no-index -U1000 ../layers/Dockerfile.1 Dockerfile.1"
 pe "docker build . -t mstage:1 -f Dockerfile.1"
 pe "docker images mstage"
 pe "docker images layers"
@@ -24,17 +24,25 @@ pe "docker image history layers:2"
 
 wait
 banner "Step 2: Copying a dependency to a final stage w/out a package manager"
-$BATCAT Dockerfile.2
+pe "$BATCAT Dockerfile.2"
 pe "docker build . -t mstage:2 -f Dockerfile.2"
 pe "docker run --rm -it mstage:2"
 
 wait
-banner "Step 2a: Find out what libraries are needed, target the builder image"
+banner "Step 2a: Find out what libraries are needed, target the builder image."
 pe "docker build -t mstage:2b -f Dockerfile.2 --target=builder ."
-pe "docker run --rm -it --entrypoint sh -u root mstage:2b"
+pe "docker run --rm --entrypoint sh -u root mstage:2b -c 'apk info --no-cache -L jq'"
+pe "docker run --rm --entrypoint sh -u root mstage:2b -c 'apk add --no-cache cmd:ldd && ldd /usr/bin/jq'"
 
 wait
 banner "Step 2b: Copy the .so file(s) to the final image"
-$BATCAT Dockerfile.3
+pe "git diff --no-index -U1000 Dockerfile.2 Dockerfile.3"
 pe "docker build . -t mstage:3 -f Dockerfile.3"
 pe "docker run --rm -it mstage:3"
+
+wait
+banner "Step 2c: Use the base-chroot method instead"
+pe "git diff --no-index -U1000 Dockerfile.3 Dockerfile.4"
+pe "docker build . -t mstage:4 -f Dockerfile.4"
+pe "docker run --rm -it mstage:4"
+pe "docker images mstage"

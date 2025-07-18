f7921190eeb9d3895d9bcddd9654db5759fbef24
Eric Smalling <eric.smalling@chainguard.dev>
2025-07-18 09:59:49 -0500

    Add advanced section
    Signed-off-by: Eric Smalling <eric.smalling@chainguard.dev>

--- containers-102/advanced/advanced.sh
+++ containers-102/advanced/advanced.sh
@@ -1,27 +1,28 @@
 #! env bash
 . ../../base.sh
+
+for i in $(docker images -q advanced); do
+  docker rmi $i
+done
+
 clear
-banner "Step 1:   "
+banner "Idea 1: Simple COPY command to copy a file from a builder image to a final image"
 $BATCAT Dockerfile.1
 pe "docker build . -t advanced:1 -f Dockerfile.1"
 pe "docker images advanced"
-
-pe "docker image history mstage:1"
-pe "docker image history layers:2"
+pe "docker image history advanced:1"
 
 wait
-banner "Step 2: Copying a dependency to a final stage w/out a package manager"
+banner "Idea 2: Using the -dev tag variant and removing packages"
 $BATCAT Dockerfile.2
-pe "docker build . -t mstage:2 -f Dockerfile.2"
-pe "docker run --rm -it mstage:2"
+pe "docker build . -t advanced:2 -f Dockerfile.2"
+pe "docker images advanced"
+pe "docker image history advanced:2"
 
 wait
-banner "Step 2a: Find out what libraries are needed, target the builder image"
-pe "docker build -t mstage:2b -f Dockerfile.2 --target=builder ."
-pe "docker run --rm -it --entrypoint sh -u root mstage:2b"
-
-wait
-banner "Step 2b: Copy the .so file(s) to the final image"
+banner "Idea 3: Using a chroor'ed apk add"
 $BATCAT Dockerfile.3
-pe "docker build . -t mstage:3 -f Dockerfile.3"
-pe "docker run --rm -it mstage:3"
+pe "docker build . -t advanced:3 -f Dockerfile.3"
+pe "docker images advanced"
+pe "docker image history advanced:3"
+

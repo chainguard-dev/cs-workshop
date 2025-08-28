c476a8751ffa0683042cd772e27dac5cb5f179d2
Eric Smalling <eric.smalling@chainguard.dev>
2025-08-28 17:29:10 -0500

    Adding distroless debugging demomagic scripts
    Signed-off-by: Eric Smalling <eric.smalling@chainguard.dev>

---
+++ debugging-distroless/k8s.sh
@@ -0,0 +1,30 @@
+#! env bash
+. ../base.sh
+
+clear
+banner "Step 0: Checking that Kubernetes cluster is running"
+# Check for a kind cluster named debugging exists and if it doesn't start one
+if ! kind get clusters | grep -q debugging; then
+  kind create cluster --name debugging
+else
+  # make sure kubectl is set to the kind cluster context
+  kubectl config use-context kind-debugging
+  # make sure the nginx-chainguard pod is destroyed
+  kubectl delete pod nginx-chainguard
+fi
+
+banner "Step 1: Start a container to debug"
+pe "kubectl run nginx-chainguard --image=cgr.dev/chainguard/nginx:latest"
+pe "kubectl wait --for=condition=ready pod/nginx-chainguard"
+pe "kubectl get pod nginx-chainguard"
+pe "kubectl exec -it nginx-chainguard -- /bin/sh"
+pe "kubectl exec -it nginx-chainguard -- /bin/bash"
+
+banner "Step 2: Start a debug container attached to the target"
+pe "kubectl debug -it nginx-chainguard --image=busybox:latest --target=nginx-chainguard"
+
+banner "Step 3: Use a variant with correct UID"
+pe "kubectl debug -it nginx-chainguard --image=cgr.dev/chainguard/busybox:latest --target=nginx-chainguard -- /bin/sh"
+
+banner "Step 4: Use sysadmin profile"
+pe "kubectl debug -it nginx-chainguard --image=busybox:latest --target=nginx-chainguard --profile sysadmin -- /bin/sh"
---
+++ debugging-distroless/local.sh
@@ -0,0 +1,28 @@
+#! env bash
+. ../base.sh
+
+CHOMP=$(docker rm -f my-chainguard)
+
+clear
+banner "Step 1: Start a container to debug"
+pe "docker run -d --name my-chainguard -p 8080:8080 cgr.dev/chainguard/nginx"
+pe "docker ps -l"
+
+banner "Step 2: Exec into that container"
+pe "docker exec -it my-chainguard sh"
+
+banner "Step 3: Start a debug container attached to the target"
+pe "docker run --rm -it --pid container:my-chainguard --network container:my-chainguard busybox"
+
+banner "Step 4: Start a debug container with the same UID as the target"
+pe "docker run --rm -it --pid container:my-chainguard --network container:my-chainguard --user 65532:65532 busybox"
+
+banner "Step 5: Start a debug container in priveleged mode"
+pe "docker run --rm -it --pid container:my-chainguard --network container:my-chainguard --privileged busybox"
+
+banner "Extras: docker diff and cp"
+pe "docker diff my-chainguard"
+pe "docker cp my-chainguard:/var/lib/nginx/html/index.html index.html"
+p "$BATCAT index.html"
+$BATCAT index.html
+
